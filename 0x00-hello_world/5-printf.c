08871990030bc11f9399d45546736f5351ba918e
Akinsbay <adebayodavidakintola@gmail.com>
2022-09-11 12:59:47 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:print the string in the printf and output the return to be zero
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
