4e8b665b3eed1be021c923d9b67eda9285ac4090
Akinsbay <adebayodavidakintola@gmail.com>
2022-09-11 17:31:47 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,8 +18,6 @@ int main(void)
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
-}
-{
 	fprintf(stderr, "%s", "[Anything]");
+	return (0);
 }
