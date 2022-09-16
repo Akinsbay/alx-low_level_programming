6d87fa424278a8d9276ffebec6745367b9a1c6b1
Akinsbay <adebayodavidakintola@gmail.com>
2022-09-16 05:48:29 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@ void more_numbers(void)
 	int c;
 	int a;
 
-	for (c = 0; c < 10; c++)
+	for (a = 0; a < 10; a++)
 	{
-		for (a = 0; a <= 14, a++)
+		for (c = 0; c <= 14, c++)
 		{
-			if (a > 9)
+			if (c > 9)
 			{
-				_putchar((a / 10) + '0');
+				_putchar((c / 10) + '0');
 			}
-			_putchar((a % 10) + '0');
+			_putchar((c % 10) + '0');
 		}
-		_putchar(0);
+		_putchar(10);
 	}
 }
