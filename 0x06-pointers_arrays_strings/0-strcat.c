92bedd71ffd54ac9fba3d4f16d79c8c2a4e4cf3a
Akinsbay <adebayodavidakintola@gmail.com>
2022-09-22 21:41:15 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncat - to concatenates two strings
+ *@dest: first string
+ *@src: second string
+ *Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int k = 0, j = 0;
+
+	while (dest[k] != '\0')
+		k++;
+
+	while (src[j] != '\0')
+	{
+		dest[k] = src[j];
+		k++;
+		j++;
+	}
+
+	dest[k] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme on how to use more of pointers, arrays and strings
