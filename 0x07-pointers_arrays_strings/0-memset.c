e50929178f0a5ed3314bdfac85441e610d5b8c68
Akinsbay <adebayodavidakintola@gmail.com>
2022-09-27 08:16:29 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - to assign memory location to n byte
+ * @s: A pointer to the memory area to be filled
+ * @b: The character to fill the memory area with
+ * @n: The number of bytes to be filled
+ * Return: pointer to filled memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int lp;
+	unsigned char *memory = s, value = b;
+
+	for (lp = 0; lp < n; lp++)
+		memory[lp] = value;
+
+	return (memory);
+}
